fc975f17aa3b701e38e3de53847b3f5f5498fb7d
Arnaud Jopart <arnaud.jopart@gmail.com>
2016-07-24 23:59:13 +0200

    Final static todos

--- server.js
+++ server.js
@@ -15,9 +15,19 @@ app.get('/todos',function(req,res){
   var filteredTodos = todos;
   if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
     filteredTodos = _.where(filteredTodos,{completed:true});
+    }
+
   }else if (queryParams.hasOwnProperty('completed')
     && queryParams.completed === 'false'){
     filteredTodos = _.where(filteredTodos,{completed:false});
+
+
+  }
+
+  if(queryParams.hasOwnProperty('q') && queryParams.q.trim().length>0){
+    filteredTodos= _.filter(filteredTodos,function(todo){
+        return todo.description..toLowerCase()indexOf(queryParams.q.toLowerCase())>-1;
+    });
   }
   res.json(filteredTodos);
 
