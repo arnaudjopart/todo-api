0bef27d816f98c0057421e341d7da4917d0066e1
Arnaud Jopart <arnaud.jopart@gmail.com>
2016-07-24 12:57:05 +0200

    todos added to app

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 var express = require('express');
 var app = express();
 
+var todos =[{
+  id:1,
+  description:"Meet mom for lunch",
+  completed:false
+},{
+  id:2,
+  description:"Go to market",
+  completed:false
+},{
+  id:3,
+  description:"Go to theator",
+  completed:true
+}];
+
+var index = 1;
+
+app.get('/todos',function(req,res){
+  res.json(todos);
+});
+
+app.get('/todos'+index,function(req,res){
+
+});
 var PORT = process.env.PORT||3000;
 
 app.get('/',function(req,res){
