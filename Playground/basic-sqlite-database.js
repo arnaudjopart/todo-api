3e97a0e067a1f9b295f9a6b84b1894a822a9a835
arnaudjopart <arnaud.jopart@gmail.com>
2016-07-25 12:07:45 +0200

    Added sequilize and sqlite demo

---
+++ Playground/basic-sqlite-database.js
@@ -0,0 +1,57 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined,undefined,undefined,{
+  'dialect':'sqlite',
+  'storage':'basic-sqlite-database.sqlite'
+});
+
+var Todo = sequelize.define('todo',{
+  description:{
+    type: Sequelize.STRING,
+    allowNull:false,
+    validate:{
+      len:[1,250]
+    }
+  },
+  completed:{
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  }
+});
+
+sequelize.sync().then(function(){
+  console.log('Everything is synced');
+  return Todo.findById(3);
+}).then(function(todo){
+  console.log(todo.toJSON());
+}).catch(function(e){
+  console.log("No todo found.");
+});
+  // Todo.create({
+  //   description: 'Take out trash'
+  // }).then(function(todo){
+  //   console.log("Finished");
+  //   console.log(todo);
+  //   return Todo.create({
+  //     description:'Clean office'
+  //   }).then(function(){
+  //     //return Todo.findById(1);
+  //     return Todo.findAll({
+  //       where:{
+  //         description:$like:'%trash%'
+  //       }
+  //     })
+  //   }).then(function(todos){
+  //     if(todos){
+  //       todos.forEach(function(todo){
+  //         console.log(todo.toJSON());
+  //       });
+  //
+  //     }else{
+  //       console.log('no todo found');
+  //     }
+  //   })
+  // }).catch(function(e){
+  //   console.log(e);
+  // })
+//});
